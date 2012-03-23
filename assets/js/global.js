c3ae289d74748c5eaa657fb9e05dad69c76da780
Greg Moser <greg@gregmoser.com>
2012-03-22 18:07:52 -0700

    some bugfixes to table expanding.

--- assets/js/global.js
+++ assets/js/global.js
@@ -6,6 +6,8 @@
  * 
  */
 
+var ajaxlock = 0;
+
 jQuery(function($){
 	
 	$('.modalload').click(function(e){
@@ -21,60 +23,80 @@ jQuery(function($){
 	
 	$('.table-expand').click(function(e){
 		e.preventDefault();
-		
-		var data = {};
-		data[ 'slatAction' ] = $(this).data('expandaction');
-		data[ 'F:' + $(this).data('parentidproperty') ] = $(this).data('parentid');
-		data[ 'propertyIdentifiers' ] = $(this).data('propertyidentifiers');
-		
-		var parentID = $(this).data('parentid');
-		var parentIDProperty  = $(this).data('parentidproperty');
-		var idProperty = $(this).data('idproperty');
-		var parentDepth = $(this).data('depth');
-		var parentIcon = $(this).children('.icon-plus');
-		
-		$.ajax({
-			url: '/plugins/Slatwall/',
-			data: data,
-			dataType: 'json',
-			contentType: 'application/json',
-			success: function(r) {
-				$.each(r["RECORDS"], function(r, rv){
-					
-					var newRow = $('#' + parentID ).clone( true );
-					$(newRow).attr('ID', rv[ idProperty ])
-					
-					$.each(rv, function(p, pv) {
-						if($(newRow).children('.' + p).children('.table-expand').length) {
-							
-							var newIcon = $(newRow).children('.' + p).children('.table-expand').clone( true );
-							$(newIcon).attr('data-depth', parentDepth + 1);
-							$(newIcon).attr('data-parentid', rv[ idProperty ]);
-							$(newIcon).removeClass('depth' + parentDepth);
-							$(newIcon).addClass('depth' + (parentDepth + 1));
-							
-							$(newRow).children('.' + p).html( newIcon );
-							$(newRow).children('.' + p).append( ' ' + pv );
-						} else {
-							$(newRow).children('.' + p).html(pv);	
-						}
-					});
-					
-					$.each($(newRow).children('.admin').children('a'), function(i, v) {
-						$(v).attr('href', $(v).attr('href').replace(parentID, rv[idProperty]));
-					});
-					
-					$('#' + parentID ).after(newRow);
-				});
-				
-				$(parentIcon).removeClass('icon-plus');
-				$(parentIcon).addClass('icon-minus');
-			},
-			error: function(r) {
-				alert('Error Loading');
-			}
-		});
-		
+		toggleTable( this );
 	});
 	
 });
+
+function toggleTable( toggleLink ) {
+	if(ajaxlock == 0) {
+		ajaxlock = 1;
+		
+		if( $(toggleLink).hasClass('open') ) {
+			
+			//$(toggleLink).removeClass('open');
+			ajaxlock = 0;
+		} else {
+			
+			var data = {};
+			data[ 'slatAction' ] = $(toggleLink).data('expandaction');
+			data[ 'F:' + $(toggleLink).data('parentidproperty') ] = $(toggleLink).data('parentid');
+			data[ 'propertyIdentifiers' ] = $(toggleLink).data('propertyidentifiers');
+			
+			var parentID = $(toggleLink).data('parentid');
+			var parentIDProperty  = $(toggleLink).data('parentidproperty');
+			var idProperty = $(toggleLink).data('idproperty');
+			var parentDepth = $(toggleLink).data('depth');
+			var parentIcon = $(toggleLink).children('.icon-plus');
+			
+			$.ajax({
+				url: '/plugins/Slatwall/',
+				data: data,
+				dataType: 'json',
+				contentType: 'application/json',
+				success: function(r) {
+					$.each(r["RECORDS"], function(r, rv){
+						
+						var newRow = $('#' + parentID ).clone( true );
+						$(newRow).attr('ID', rv[ idProperty ])
+						
+						$.each(rv, function(p, pv) {
+							if($(newRow).children('.' + p).children('.table-expand').length) {
+								
+								var newIcon = $(newRow).children('.' + p).children('.table-expand').clone( true );
+								
+								$(newIcon).data('depth', parentDepth + 1);
+								$(newIcon).data('parentid', rv[ idProperty ]);
+								
+								$(newIcon).removeClass('depth' + parentDepth);
+								$(newIcon).addClass('depth' + (parentDepth + 1));
+								
+								$(newRow).children('.' + p).html( newIcon );
+								$(newRow).children('.' + p).append( ' ' + pv );
+							} else {
+								$(newRow).children('.' + p).html(pv);	
+							}
+						});
+						
+						$.each($(newRow).children('.admin').children('a'), function(i, v) {
+							$(v).attr('href', $(v).attr('href').replace(parentID, rv[idProperty]));
+						});
+						
+						$('#' + parentID ).after(newRow);
+					});
+					
+					$(toggleLink).addClass('open');
+					$(parentIcon).removeClass('icon-plus');
+					$(parentIcon).addClass('icon-minus');
+					
+					ajaxlock = 0;
+				},
+				error: function(r) {
+					alert('Error Loading');
+					
+					ajaxlock = 0;
+				}
+			});
+		}
+	}
+}
