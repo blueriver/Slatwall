76dcdd8f927f920b64b0dcec90ef97fb576a70a6
Greg Moser <greg@gregmoser.com>
2012-12-23 14:19:34 -0800

    Started building slatwall jQuery plugin

---
+++ assets/js/jquery-slatwall.js
@@ -0,0 +1,48 @@
+(function( $ ){
+	var methods = {
+		init : function( options ) {
+			return this.each(function(){
+				var $this = $(this),
+				data = $this.data('tooltip'),
+				tooltip = $('<div />', {
+					text : $this.attr('title')
+				});
+				// If the plugin hasn't been initialized yet
+				if ( ! data ) {
+					/*
+					Do more setup stuff here
+					*/
+					$(this).data('tooltip', {
+						target : $this,
+						tooltip : tooltip
+					});
+				}
+			});
+		},
+		destroy : function( ) {
+			return this.each(function(){
+				var $this = $(this),
+				data = $this.data('tooltip');
+
+				// Namespacing FTW
+				$(window).unbind('.tooltip');
+				data.tooltip.remove();
+				$this.removeData('tooltip');
+			})
+		},
+		updateSmartList : function( ) {
+			
+		}
+	};
+
+	$.fn.slatwall = function( method ) {
+		if ( methods[method] ) {
+			return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+		} else if ( typeof method === 'object' || ! method ) {
+			return methods.init.apply( this, arguments );
+		} else {
+			$.error( 'Method ' +  method + ' does not exist on jQuery.slatwall' );
+		}  
+	};
+
+})( jQuery );
